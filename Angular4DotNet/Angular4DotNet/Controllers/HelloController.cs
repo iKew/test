e4004b5b0579c4004af023fc399ab4ce5460584b
ikew <k-loveza@hotmail.com>
2015-06-08 18:09:38 +0700

    Creating an Angular View inside and MVC View

---
+++ Angular4DotNet/Angular4DotNet/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Angular4DotNet.Controllers
+{
+    public class HelloController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index","","you");
+        }
+
+    }
+}
