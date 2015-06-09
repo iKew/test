cbc6c3a93ddc132f076610a67c7b64b4be880551
ikew <k-loveza@hotmail.com>
2015-06-09 14:10:59 +0700

    Creating a Mini SPA Inside an MVC View - Part 1

---
+++ Angular4DotNet/Angular4DotNet/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using Angular4DotNet.Models.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular4DotNet.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private RegistrationVmBuilder _registrationVmBuilder = new RegistrationVmBuilder();
+        public ActionResult Index()
+        {
+            var data = _registrationVmBuilder.BuildRegistrationVm();
+            return View(data);
+        }
+
+    }
+}
--- Angular4DotNet/Angular4DotNet/Models/Registration/RegistrationVm.cs
+++ Angular4DotNet/Angular4DotNet/Models/Registration/RegistrationVm.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Angular4DotNet.Models
+﻿
+namespace Angular4DotNet.Models.Registration
 {
     public class RegistrationVm
     {
