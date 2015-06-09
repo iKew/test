c90cc9093eca62eb941fd14fbdc244fb0b951437
ikew <k-loveza@hotmail.com>
2015-06-09 15:18:17 +0700

    end Using Angular with ASP.NET MVC

--- Angular4DotNet/Angular4DotNet/App_Start/RouteConfig.cs
+++ Angular4DotNet/Angular4DotNet/App_Start/RouteConfig.cs
@@ -13,10 +13,27 @@ namespace Angular4DotNet
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Registration",
+            //    url: "Registration/{*catchall}",
+            //    defaults: new { controller = "Registration", action = "Index" }
+            //);
+            routes.MapRoute(
+                name: "Registration Courses",
+                url: "Registration/Courses",
+                defaults: new { controller = "Registration", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Registration Instructors",
+                url: "Registration/Instructors",
+                defaults: new { controller = "Registration", action = "Index" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Registration", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
