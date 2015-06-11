41e7752f7edec9293cb715f38269cb484c42e858
ikew <k-loveza@hotmail.com>
2015-06-11 13:45:11 +0700

    SignalR by asp.net NOT angular

---
+++ SignalR/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
---
+++ SignalR/SignalR/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(SignalR.StartUp))]
+namespace SignalR
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app) { 
+            app.MapSignalR();
+        }
+    }
+}
