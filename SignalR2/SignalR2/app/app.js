5a8bb755b18843f9f408318dd2afed8f4cada01e
ikew <k-loveza@hotmail.com>
2015-06-11 16:57:43 +0700

    Signal unsuccessful 3

---
+++ SignalR2/SignalR2/app/app.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    angular.module('app', []);
+
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+
+    $.connection.hub.error(function (err) {
+        console.log('An error occurred: ' + err);
+    });
+
+    angular.module('app')
+        .value('chat', $.connection.chat)
+        .value('toaster', toastr);
+})();
